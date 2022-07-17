c6598f727ecad158627be7ea2308baa30378dd0d
AshishThakur10 <sendashishthakur@gmail.com>
2022-07-17 23:26:21 +0530

    update userModel

--- Model/userModel.js
+++ Model/userModel.js
@@ -113,10 +113,14 @@ const userSchema = new mongoose.Schema(
       unique: true,
       required: true,
     },
-    share: [{
+    stock: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: "Share",
-    }]
+    }],
+    notification: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "notification",
+    }],
   },
   { timestamps: true }
 );
