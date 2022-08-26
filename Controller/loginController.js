bba31fa8cf26181dd5fc24b3a5d3d1b67e5749d7
Ashish Thakur <sendashishthakur@gmail.com>
2022-08-27 01:06:11 +0530

    new password

--- Controller/loginController.js
+++ Controller/loginController.js
@@ -4,18 +4,25 @@ const crypto = require("crypto");
 exports.login = async (req, res, next) => {
     try {
       let token = crypto.randomBytes(8).toString("hex");
-      const loginData = {
-        mobile_number: req.body.mobile_number,
-        email_id: req.body.email_id,
-        password: req.body.password,
-        token: token, 
-      } 
-
-     
-    // const data = await Login.create(loginData);
-      res.send({
-        message: loginData,
+      let userPassword = req.body.password;
+      if(userPassword == "Aveek@321@agoi"){
+        const loginData = {
+          mobile_number: req.body.mobile_number,
+          email_id: req.body.email_id,
+          password: req.body.password,
+          token: token, 
+        } 
+  
+       
+      // const data = await Login.create(loginData);
+       return res.send({
+          message: loginData,
+        })
+      }
+      return res.send({
+        message: "wrong credentials",
       })
+      
     } catch (err) {
         res.send({
             message: err.message,
